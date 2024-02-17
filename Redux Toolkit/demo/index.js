d09463e7906361af75048dca7bc9cdc02b5d8980
Ankit <ankitchauhan21500@gmail.com>
2024-02-17 16:57:09 +0530

    tutorial 23 || perdorming async functions

--- Redux Toolkit/demo/index.js
+++ Redux Toolkit/demo/index.js
@@ -2,25 +2,27 @@
 const store = require("../app/store");
 
 // todo : Importing cake action creators from the cake slice
-const {cakeActions} = require("../features/cake/cakeSlice");
-const iceCreamActions = require("../features/iceCream/iceCreamSlice").iceCreamActions
-
+const { cakeActions } = require("../features/cake/cakeSlice");
+const { iceCreamActions } = require("../features/iceCream/iceCreamSlice")
+const { fetchUser } = require("../features/user/userSlice");
+ 
 // Subscribing to store updates and logging the updated state
-const unSubscribe = store.subscribe(() => {
+const unSubscribe = store.subscribe(() => { 
     // ! we dont need to add this console.log here as middleware will take care of this
     // console.log("Updated State: ", store.getState());
-});
-
-// Dispatching actions to simulate cake ordering and restocking
-store.dispatch(cakeActions.ordered());
+}); 
+ 
+// todo : Dispatching actions to simulate cake ordering and restocking
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(200));
 
-// todo : for iceCream  
-console.log("\n")     
+// todo : for iceCream        
+console.log("\n")
 store.dispatch(iceCreamActions.ordered());
 store.dispatch(iceCreamActions.restocked(300));
  
+store.dispatch(fetchUser()); 
 // Unsubscribing from store updates
-unSubscribe(); 
-      
+unSubscribe();  
+ 
+
